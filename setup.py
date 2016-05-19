569c8527b768041563117709f975be29f9f2b3a5
Ben Mather <bwhmather@bwhmather.com>
2016-05-19 22:33:28 +0100

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='dug',
     url='https://github.com/bwhmather/python-dug',
-    version='0.0.1',
+    version='0.1.0',
     author='Ben Mather',
     author_email='bwhmather@bwhmather.com',
     maintainer='',
